14958fd87db608844e37815e4a9b80f70938e4a1
Greg Ogun <gregogun97@gmail.com>
2021-09-08 19:04:13 +0100

    feat: setup link token creation

---
+++ pages/api/plaid/link.js
@@ -0,0 +1,27 @@
+import { plaidClient } from '@/lib/plaid';
+import { formatError } from '@/utils/formatError';
+
+export default async (req, res) => {
+  const request = {
+    user: {
+      client_user_id: 'user-id'
+    },
+    client_name: 'Aqua Bank',
+    language: 'en',
+    products: ['auth', 'transactions'],
+    country_codes: ['GB'],
+    account_filters: {
+      depository: {
+        account_subtypes: ['checking']
+      }
+    }
+  };
+
+  try {
+    const response = await plaidClient.linkTokenCreate(request);
+    const linkToken = response.data.link_token;
+    return res.status(200).json({ linkToken });
+  } catch (error) {
+    return res.status(500).json(formatError);
+  }
+};
