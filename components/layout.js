61eab28d05e6038695dbc5a0e831a57e13eb708b
Greg Ogun <gregogun97@gmail.com>
2021-09-11 22:26:12 +0100

    style: minor adjustments

--- components/layout.js
+++ components/layout.js
@@ -6,17 +6,18 @@ const Nav = () => {
       bg="gray.1"
       borderRadius="md"
       h="100%"
-      p="2rem 8rem 2rem 1rem"
+      p="2rem 6rem 2rem 2rem"
       as="aside"
       role="navigation"
     >
-      <List>
+      <List fontSize="md">
         <ListItem mb="200px">Aqua</ListItem>
         <ListItem mb="1rem">Overview</ListItem>
         <ListItem mb="1rem">Transactions</ListItem>
         <ListItem mb="1rem">Accounts</ListItem>
-        <ListItem mb="2rem">Profile</ListItem>
+        <ListItem mb="2rem">Analytics</ListItem>
         <Divider h="1px" w="100%" bg="gray.2" mb="2rem" />
+        <ListItem mb="1rem">Profile</ListItem>
         <ListItem>Settings</ListItem>
       </List>
     </VStack>
@@ -25,7 +26,15 @@ const Nav = () => {
 
 const Layout = ({ children }) => {
   return (
-    <Flex w="100vw" h="100vh" boxSizing="border-box" p="1rem">
+    <Flex
+      mx="auto"
+      w="100vw"
+      h="100vh"
+      boxSizing="border-box"
+      p="1rem 1rem 0 1rem"
+      maxW="1440px"
+      overflow="hidden"
+    >
       <Nav />
       {children}
     </Flex>
