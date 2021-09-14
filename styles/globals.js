7a97e1a3b815a1c0a5f9d46d4d113088744d8f7b
Greg Ogun <gregogun97@gmail.com>
2021-09-14 04:47:34 +0100

    fix(temp): remove color mode props, fixes dark mode persisting

--- styles/globals.js
+++ styles/globals.js
@@ -1,20 +1,20 @@
 export const styles = {
   global: (props) => ({
     '*, *::before, *::after': {
-      boxSizing: 'inherit'
+      boxSizing: 'inherit',
     },
     body: {
       overflowX: 'hidden',
-      bg: props.colorMode === 'light' ? 'default.white' : 'default.black',
-      color: props.colorMode === 'light' ? 'default.black' : 'default.white',
-      boxSizing: 'border-box'
+      bg: 'default.white',
+      color: 'default.black',
+      boxSizing: 'border-box',
     },
     div: {
       borderRadius: 'sm',
-      boxSizing: 'inherit'
+      boxSizing: 'inherit',
     },
     li: {
-      listStyleType: 'none'
-    }
-  })
+      listStyleType: 'none',
+    },
+  }),
 };
